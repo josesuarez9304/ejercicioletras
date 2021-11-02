72d1b088604fed6b11ee020dd20ee23d1f91f5b6
josesuarez9304 <josesuarezman1@gmail.com>
2021-11-02 17:39:46 -0500

    quedo listo para el profe

--- script.js
+++ script.js
@@ -38,23 +38,21 @@ const fnA1 = () => {
   console.log(letters);
   if (letters.innerHTML == "") {
     letters.innerHTML = "A";
-  }else  if(letters.innerHTML == "ABC"){
-    numbers.innerHTML = "1"; 
-  
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
+    numbers.innerHTML = "1";
   }
 };
 
 const fnB2 = () => {
   if (letters.innerHTML == "A") {
     letters.innerHTML += "B";
-  } else if(numbers.innerHTML == "" && letters.innerHTML == "" ) {
+  } else if (numbers.innerHTML == "" && letters.innerHTML == "") {
     alert("faltan letras");
   }
 
-
-  if(numbers.innerHTML == "1" ){
-    numbers.innerHTML +="2"
-  }else if(letters.innerHTML == "ABC"){
+  if (numbers.innerHTML == "1") {
+    numbers.innerHTML += "2";
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
     alert("faltan numeros");
   }
   // letters.innerHTML = letters.innerHTML +"B";e s ihualñ a lo d eabajo
@@ -62,35 +60,191 @@ const fnB2 = () => {
 };
 const fnC3 = () => {
   console.log(letters.innerHTML);
-    if (letters.innerHTML == "AB") {
-        letters.innerHTML += "C";
-      } else if( numbers.innerHTML == "" && ( letters.innerHTML == ""    ||     letters.innerHTML == "A" || letters.innerHTML =="AB") ) {
-        alert("faltan letras");
-      }
+  if (letters.innerHTML == "AB") {
+    letters.innerHTML += "C";
+  } else if (
+    numbers.innerHTML == "" &&
+    (letters.innerHTML == "" ||
+      letters.innerHTML == "A" ||
+      letters.innerHTML == "AB")
+  ) {
+    alert("faltan letras");
+  }
 
-      if(numbers.innerHTML =="12"){
-        numbers.innerHTML +="3";
-      }else if(letters.innerHTML == "ABC"){
-        alert("faltan numeros");
-      }
-
+  if (numbers.innerHTML == "12") {
+    numbers.innerHTML += "3";
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
+    alert("faltan numeros");
+  }
 };
 
+const fnD4 = () => {
+  console.log(letters.innerHTML);
+  if (letters.innerHTML == "ABC") {
+    letters.innerHTML += "D";
+  } else if (
+    numbers.innerHTML == "" &&
+    (letters.innerHTML == "" ||
+      letters.innerHTML == "A" ||
+      letters.innerHTML == "AB" ||
+      letters.innerHTML == "ABC")
+  ) {
+    alert("faltan letras");
+  }
 
-const fnD4 = () => {};
-const fnE5 = () => {};
-const fnF6 = () => {};
-const fnG7 = () => {};
-const fnH8 = () => {};
-const fnI9 = () => {};
+  if (numbers.innerHTML == "123") {
+    numbers.innerHTML += "4";
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
+    alert("faltan numeros");
+  }
+};
+
+const fnE5 = () => {
+  console.log(letters.innerHTML);
+  if (letters.innerHTML == "ABCD") {
+    letters.innerHTML += "E";
+  } else if (
+    numbers.innerHTML == "" &&
+    (letters.innerHTML == "" ||
+      letters.innerHTML == "A" ||
+      letters.innerHTML == "AB" ||
+      letters.innerHTML == "ABC" ||
+      letters.innerHTML == "ABCD")
+  ) {
+    alert("faltan letras");
+  }
+
+  if (numbers.innerHTML == "1234") {
+    numbers.innerHTML += "5";
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
+    alert("faltan numeros");
+  }
+};
+const fnF6 = () => {
+  console.log(letters.innerHTML);
+  if (letters.innerHTML == "ABCDE") {
+    letters.innerHTML += "F";
+  } else if (
+    numbers.innerHTML == "" &&
+    (letters.innerHTML == "" ||
+      letters.innerHTML == "A" ||
+      letters.innerHTML == "AB" ||
+      letters.innerHTML == "ABC" ||
+      letters.innerHTML == "ABCD" ||
+      letters.innerHTML == "ABCDE")
+  ) {
+    alert("faltan letras");
+  }
+
+  if (numbers.innerHTML == "12345") {
+    numbers.innerHTML += "6";
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
+    alert("faltan numeros");
+  }
+};
+const fnG7 = () => {
+  console.log(letters.innerHTML);
+  if (letters.innerHTML == "ABCDEF") {
+    letters.innerHTML += "G";
+  } else if (
+    numbers.innerHTML == "" &&
+    (letters.innerHTML == "" ||
+      letters.innerHTML == "A" ||
+      letters.innerHTML == "AB" ||
+      letters.innerHTML == "ABC" ||
+      letters.innerHTML == "ABCD" ||
+      letters.innerHTML == "ABCDE" ||
+      letters.innerHTML == "ABCDEF")
+  ) {
+    alert("faltan letras");
+  }
+
+  if (numbers.innerHTML == "123456") {
+    numbers.innerHTML += "7";
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
+    alert("faltan numeros");
+  }
+};
+const fnH8 = () => {
+  console.log(letters.innerHTML);
+  if (letters.innerHTML == "ABCDEFG") {
+    letters.innerHTML += "H";
+  } else if (
+    numbers.innerHTML == "" &&
+    (letters.innerHTML == "" ||
+      letters.innerHTML == "A" ||
+      letters.innerHTML == "AB" ||
+      letters.innerHTML == "ABC" ||
+      letters.innerHTML == "ABCD" ||
+      letters.innerHTML == "ABCDE" ||
+      letters.innerHTML == "ABCDEF" ||
+      letters.innerHTML == "ABCDEFG")
+  ) {
+    alert("faltan letras");
+  }
+
+  if (numbers.innerHTML == "1234567") {
+    numbers.innerHTML += "8";
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
+    alert("faltan numeros");
+  }
+};
+const fnI9 = () => {
+  console.log(letters.innerHTML);
+  if (letters.innerHTML == "ABCDEFGH") {
+    letters.innerHTML += "I";
+  } else if (
+    numbers.innerHTML == "" &&
+    (letters.innerHTML == "" ||
+      letters.innerHTML == "A" ||
+      letters.innerHTML == "AB" ||
+      letters.innerHTML == "ABC" ||
+      letters.innerHTML == "ABCD" ||
+      letters.innerHTML == "ABCDE" ||
+      letters.innerHTML == "ABCDEF" ||
+      letters.innerHTML == "ABCDEFG" ||
+      letters.innerHTML == "ABCDEFGH")
+  ) {
+    alert("faltan letras");
+  }
+
+  if (numbers.innerHTML == "12345678") {
+    numbers.innerHTML += "9";
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
+    alert("faltan numeros");
+  }
+};
+const fnJ0 = () => {
+  console.log(letters.innerHTML);
+  if (letters.innerHTML == "ABCDEFGHI") {
+    letters.innerHTML += "J";
+  } else if (
+    numbers.innerHTML == "" &&
+    (letters.innerHTML == "" ||
+      letters.innerHTML == "A" ||
+      letters.innerHTML == "AB" ||
+      letters.innerHTML == "ABC" ||
+      letters.innerHTML == "ABCD" ||
+      letters.innerHTML == "ABCDE" ||
+      letters.innerHTML == "ABCDEF" ||
+      letters.innerHTML == "ABCDEFG" ||
+      letters.innerHTML == "ABCDEFGH" ||
+      letters.innerHTML == "ABCDEFGHI")
+  ) {
+    alert("faltan letras");
+  }
+
+  if (numbers.innerHTML == "123456789") {
+    numbers.innerHTML += "0";
+  } else if (letters.innerHTML == "ABCDEFGHIJ") {
+    alert("faltan numeros");
+  }
+};
 const fnCL = () => {
-  letters.innerHTML ="";
-
+  letters.innerHTML = "";
 };
-const fnJ0 = () => {};
 const fnCN = () => {
-  numbers.innerHTML="";
-
+  numbers.innerHTML = "";
 };
 
 //eventos
@@ -106,16 +260,40 @@ c3.onclick = () => {
   fnC3();
 };
 
+d4.onclick = () => {
+  fnD4();
+};
+
+e5.onclick = () => {
+  fnE5();
+};
+
+f6.onclick = () => {
+  fnF6();
+};
+
+g7.onclick = () => {
+  fnG7();
+};
+
+h8.onclick = () => {
+  fnH8();
+};
+
+i9.onclick = () => {
+  fnI9();
+};
+
 //borrar letras
 cl.onclick = () => {
   fnCL();
-}
+};
 
-j0.onclick= () => {
+j0.onclick = () => {
   fnJ0();
 };
 
 //borrar numeros
 cn.onclick = () => {
   fnCN();
-}
+};
